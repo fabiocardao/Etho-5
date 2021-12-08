c6b765bb5940f92b8c138493b6793f659bef8d3d
Fábio Cardão <fabio.cardao@gmail.com>
2021-12-08 20:05:22 -0300

    lista de rounds e jogadores

--- Etho-5/force-app/main/default/lwc/meuRoundContainer/meuRoundContainer.js
+++ Etho-5/force-app/main/default/lwc/meuRoundContainer/meuRoundContainer.js
@@ -3,9 +3,25 @@ import { LightningElement, api } from 'lwc';
 export default class MeuRoundContainer extends LightningElement {
     @api round;
 
-    dinheiro = 10;
+    get premioTotal() {
+        return this.formatarMoeda(this.round.PremioTotal__c);
+    }
 
-    get formatarMoeda() {
-        return this.round.PremioTotal__c.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    get jogadores() {
+        if (this.round) {
+            return [ ...this.round.Jogadores__r.map((jogador) => {
+                return {
+                    ...jogador, valorDivida: "Valor Dívida: " + this.formatarMoeda(jogador.Candidato__r.ValorDivida__c) 
+                }
+            })];
+        }
+        return [];
+    }
+
+    formatarMoeda(valor) {
+        if (valor) {
+            return valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+        }
+        return '';
     }
 }
